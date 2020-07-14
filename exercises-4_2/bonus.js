e97eeb75ed2671908ec94815c28a9a17a6d03d96
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-14 17:20:58 -0300

    bonus

--- exercises-4_2/bonus.js
+++ exercises-4_2/bonus.js
@@ -1,4 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+//Ordene o array numbers em ordem crescente e imprima seus valores;
 
 numbers.sort(function(a, b) {
     return a - b;
@@ -6,8 +9,30 @@ numbers.sort(function(a, b) {
 
 console.log(numbers)
 
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+
 numbers.sort(function(a, b) {
     return b - a;
 });
 
 console.log(numbers)
+
+//Criar um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo. 
+
+//Caso não haja próximo valor, a multiplicação deverá ser feita por 2. 
+
+//Faça isso utilizando o for e o método push.
+
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+for (let i = 0; i < numbers.length-1; i++) {
+    newNumbers.push(numbers[i]*numbers[i+1]);
+}
+
+newNumbers.push(numbers[numbers.length-1]*2);
+
+console.log(newNumbers); 
+

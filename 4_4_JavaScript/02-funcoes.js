c59ab4aa85c1c3ff830d84bba553b34c0851cd05
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-16 16:14:08 -0300

    01 exercicio funcao palindromos

--- 4_4_JavaScript/02-funcoes.js
+++ 4_4_JavaScript/02-funcoes.js
@@ -1,3 +1,5 @@
+//funcao que verifica palindromos
+
 let palavra="desenvolvimento";
 
 function verificaPalindromo(palavra){
@@ -10,3 +12,21 @@ function verificaPalindromo(palavra){
 }
 
 verificaPalindromo(palavra);
+
+
+//funcao que recebe array e retorna o indice de maior valor
+
+let numbers=[2,3,6,7,10,1];
+let maior=0;
+
+function verificaMaior(numbers){
+    for (let i=0; i<numbers.length; i+=1){
+        if (numbers[i]>maior){
+            maior=numbers[i];
+        }
+    }
+    
+    console.log(numbers.indexOf(maior));
+}
+
+verificaMaior(numbers);

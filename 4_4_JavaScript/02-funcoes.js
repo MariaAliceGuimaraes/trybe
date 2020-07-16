73d44ea59e650c59acb501918c2022274fdcb4cf
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-16 16:38:28 -0300

    04  exercicio funcao nome maior

--- 4_4_JavaScript/02-funcoes.js
+++ 4_4_JavaScript/02-funcoes.js
@@ -50,3 +50,19 @@ function verificaMenor(numbers2){
 }
 
 verificaMenor(numbers2);
+
+// Funcao que recebe array de nomes e retorne o nome com maior quantidade de caracteres
+
+let nomes=["Jose","Lucas", "Nadia", "Fernanda", "Cairo", "Joana"];
+let nomeMaior="";
+
+function maiorNome(nomes){
+    for (let i=0; i<nomes.length; i+=1){
+        if (nomes[i].length>nomeMaior.length){
+            nomeMaior=nomes[i];
+        }
+    }
+    console.log(nomeMaior)
+}
+
+maiorNome(nomes);

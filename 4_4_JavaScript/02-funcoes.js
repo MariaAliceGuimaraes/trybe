4f29cb11546030468577a462e1dfa3f0e5353f9a
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-16 19:50:49 -0300

    Exercicios Java Script Objetos e Funcoes

--- 4_4_JavaScript/02-funcoes.js
+++ 4_4_JavaScript/02-funcoes.js
@@ -70,15 +70,31 @@ maiorNome(nomes);
 //05 funcao que recebe um array de inteiros e retorne o inteiro que mais se repete
 
 let numbers3=[2,3,2,5,8,2,3];
-let repetido=0;
+
 
 function maisRepetido(numbers3){
-    for (let i=0; i<numbers3.length; i+=1){
-        if(numbers3[i]==numbers3[i+1]){
-            repetido=numbers3[i+1];
+    let maior=0;
+    let objeto={};
+    let numeroRepetido;
+
+    for (let i in numbers3){
+        let n = numbers3[i];
+
+        if (objeto[n]){
+            objeto[n]+=1
         }
-    }    
-    console.log(repetido)
+        else {
+            objeto[n]=1
+        }
+    }
+
+    for (let i in objeto){
+        if (objeto[i]>maior){
+            maior=objeto[i];
+            numeroRepetido=i;
+        }
+    }
+    console.log(numeroRepetido)
 }
 
 maisRepetido(numbers3);

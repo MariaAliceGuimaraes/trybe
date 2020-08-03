c67751f45ec17e970501f0790b8c81b5a696d603
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-03 10:29:06 -0300

    exercicio 6_1

---
+++ 6_1_Forms/script.js
@@ -0,0 +1,18 @@
+
+let arrayEstados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',];
+
+function createStates (){
+
+    for (let i=0; i<arrayEstados.length; i+=1){
+
+        const estado = document.createElement("option");
+
+        estado.innerText = arrayEstados[i];
+
+        document.getElementById("estados").appendChild(estado);
+    }
+}
+
+window.onload = function(){
+    createStates();
+}
